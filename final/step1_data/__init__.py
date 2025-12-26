878c314f2df48b58075e2767ece2ef7015d0f3f6
Hieu Nguyen <hieuteo03@gmail.com>
2025-12-26 16:38:39 +0700

    chore: stop tracking final folder

--- final/step1_data/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-BƯỚC 1: LẤY DỮ LIỆU TỪ BINANCE
-"""
-
-from .fetch_data import fetch_binance_data, clear_cache
-
-__all__ = ['fetch_binance_data', 'clear_cache']
--- final/step3_model/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-BƯỚC 3: XÂY DỰNG MODEL BiLSTM
-"""
-
-from .bilstm import build_bilstm_model, print_model_summary
-
-__all__ = ['build_bilstm_model', 'print_model_summary']
